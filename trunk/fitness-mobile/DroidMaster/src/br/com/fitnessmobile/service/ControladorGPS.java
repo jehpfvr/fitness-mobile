870a456f54ea568f817e1bcf460e3915cda6ec58
thiago.abreulima@gmail.com <thiago.abreulima@gmail.com@740cbde1-dae2-8632-a52b-12e60ba9706f>
2011-08-26 14:24:13 +0000

    Modificações no modulo GPS

--- trunk/fitness-mobile/DroidMaster/src/br/com/fitnessmobile/service/ControladorGPS.java
+++ trunk/fitness-mobile/DroidMaster/src/br/com/fitnessmobile/service/ControladorGPS.java
@@ -5,5 +5,5 @@ public interface ControladorGPS {
 	public void startGPS();
 	public void stopGPS();
 	public double getDistancia();
-
+	public void setOnControladorGPS(OnControladorGPSListener listener);
 }
