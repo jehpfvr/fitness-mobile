7ddf7e9819f1903db5cf05cf8c9e9e416c4a4e13
ataide.azevedo <ataide.azevedo@gmail.com>
2011-09-09 11:24:52 +0000

    Versão criada para trabalharmos com ela no sábado, está bastante simplificada e o código com comentários, basicamente mantivemos o layout e a disposição dos menus e telas. Sábado faremos, utilizando essa versão, o "esqueleto" da aplicação inteira, e comentaremos com os "TODO" o que deve ser implementado.

---
+++ fitness-mobile/ADEV/trunk/fitnessMobileADEV/src/br/com/fitnessmobile/model/banco/Banco.java
@@ -0,0 +1,10 @@
+package br.com.fitnessmobile.model.banco;
+
+/**
+ * Classe de controle e acesso ao Banco de Dados
+ */
+@SuppressWarnings("unused")
+public class Banco {
+	private static final String DATABASE_NAME = "FitnessMobile.db";
+	private static final int DATABASE_VERSION = 1;
+}
