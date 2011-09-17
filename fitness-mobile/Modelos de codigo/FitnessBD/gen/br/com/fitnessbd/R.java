b315522d976985b2095b9ba6507a6df727935cb0
gustavobarbosaa <gustavobarbosaa@gmail.com>
2011-09-17 20:04:41 +0000

    DroidMaster Ajustes no layout da lista de programas.

--- fitness-mobile/Modelos de codigo/FitnessBD/gen/br/com/fitnessbd/R.java
+++ fitness-mobile/Modelos de codigo/FitnessBD/gen/br/com/fitnessbd/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int logo_fitness=0x7f020001;
     }
     public static final class id {
         public static final int btnCancelar=0x7f050004;
@@ -27,6 +28,7 @@ public final class R {
         public static final int main=0x7f030000;
         public static final int programa_form=0x7f030001;
         public static final int programa_lista=0x7f030002;
+        public static final int programa_lista_header=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
