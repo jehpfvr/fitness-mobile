c27707c218691c4ec2e8aab4fad4d8bb50b7a424
pedrojramancio <pedrojramancio@740cbde1-dae2-8632-a52b-12e60ba9706f>
2011-10-13 22:48:14 +0000

    Criando o projeto FitnessMobileAndroid.

---
+++ trunk/fitness-mobile/FitnessMobileAndroid/src/br/com/fitnessmobile/model/Aerobico.java
@@ -0,0 +1,48 @@
+package br.com.fitnessmobile.model;
+
+import java.io.Serializable;
+
+public class Aerobico implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private long duracao;
+	private Double distancia;
+	private Double velocidade;
+	
+	public Aerobico() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public long getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(long duracao) {
+		this.duracao = duracao;
+	}
+
+	public Double getVelocidade() {
+		return velocidade;
+	}
+
+	public void setVelocidade(Double velocidade) {
+		this.velocidade = velocidade;
+	}
+
+	public Double getDistancia() {
+		return distancia;
+	}
+
+	public void setDistancia(Double distancia) {
+		this.distancia = distancia;
+	}
+}
