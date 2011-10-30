2057e8fce244d421cc4b5511d1bdf238e298d637
gustavobarbosaa@gmail.com <gustavobarbosaa@gmail.com@740cbde1-dae2-8632-a52b-12e60ba9706f>
2011-10-30 16:10:48 +0000

    DroidMaster Ajustes no script.

--- trunk/documentos/arquitetura/banco de dados/Script_fitness_mobile.sql
+++ trunk/documentos/arquitetura/banco de dados/Script_fitness_mobile.sql
@@ -1,8 +1,8 @@
 -- -----------------------------------------------------
 -- Table exercicio
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS exercicio (
-  'exe_id' INT NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS exercicio (
+  'exe_id' INTEGER PRIMARY KEY,
   'exe_nome' VARCHAR(45) NOT NULL ,
   'exe_descricao' VARCHAR(45) NOT NULL ,
   'exe_situacao' CHAR(1) NOT NULL ,
@@ -14,21 +14,21 @@ CREATE  TABLE IF NOT EXISTS exercicio (
 -- -----------------------------------------------------
 -- Table programa
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS programa (
-  'pro_id' INT NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS programa (
+  'pro_id' INTEGER PRIMARY KEY,
   'pro_nome' VARCHAR(50) NOT NULL ,
-  'pro_data_inicial' INT  NOT NULL ,
-  'pro_data_final' INT  NOT NULL
+  'pro_data_inicial' INTEGER  NOT NULL ,
+  'pro_data_final' INTEGER  NOT NULL
 );
 
 
 -- -----------------------------------------------------
 -- Table pessoa
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS pessoa (
-  'pes_id' INT NOT NULL PRIMARY KEY ,
+CREATE TABLE IF NOT EXISTS pessoa (
+  'pes_id' INTEGER PRIMARY KEY,
   'pes_nome' VARCHAR(45) NOT NULL ,
-  'pes_data_nasc' INT  NOT NULL ,
+  'pes_data_nasc' INTEGER  NOT NULL ,
   'pes_peso' VARCHAR(45) NULL ,
   'pes_altura' VARCHAR(45) NULL ,
   'pes_sexo' VARCHAR(1) NOT NULL
@@ -38,24 +38,22 @@ CREATE  TABLE IF NOT EXISTS pessoa (
 -- -----------------------------------------------------
 -- Table medida
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS medida (
-  'med_id' INT NOT NULL ,
-  'med_data_cadastro' INT  NOT NULL ,
-  'med_pes_id' INT NOT NULL ,
-  'med_musculo' INT NOT NULL ,
+CREATE TABLE IF NOT EXISTS medida (
+  'med_id' INTEGER,
+  'med_data_cadastro' INTEGER  NOT NULL ,
+  'med_pes_id' INTEGER NOT NULL ,
+  'med_musculo' INTEGER NOT NULL ,
   PRIMARY KEY ('med_id', 'med_pes_id') ,
   CONSTRAINT 'fk_medida_pessoa'
     FOREIGN KEY ('med_pes_id' )
-    REFERENCES pessoa ('pes_id' )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES pessoa ('pes_id' ));
 
 
 -- -----------------------------------------------------
 -- Table configuracao
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS configuracao (
-  'con_id' INT NOT NULL ,
+  'con_id' INTEGER,
   'con_metragem' VARCHAR(45) NOT NULL ,
   'con_kilometragem' VARCHAR(45) NOT NULL ,
   'con_pesagem' VARCHAR(45) NOT NULL ,
@@ -66,81 +64,69 @@ CREATE  TABLE IF NOT EXISTS configuracao (
 -- Table etapa
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS etapa (
-  'eta_id' INT NOT NULL ,
+  'eta_id' INTEGER,
   'eta_nome' VARCHAR(50) NOT NULL ,
-  'eta_pro_id' INT NOT NULL ,
-  PRIMARY KEY ('eta_id') ,
+  'eta_pro_id' INTEGER NOT NULL ,
+  PRIMARY KEY ('eta_id'),
   CONSTRAINT 'fk_etapa_programa'
     FOREIGN KEY ('eta_pro_id' )
-    REFERENCES programa ('pro_id' )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES programa ('pro_id' ));
 
 
 -- -----------------------------------------------------
 -- Table etapa_exercicio
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS etapa_exercicio (
-  'ete_id' INT NOT NULL ,
-  'ete_eta_id' INT NOT NULL ,
-  'ete_exe_id' INT NOT NULL ,
+  'ete_id' INTEGER,
+  'ete_eta_id' INTEGER NOT NULL ,
+  'ete_exe_id' INTEGER NOT NULL ,
   PRIMARY KEY ('ete_id') ,
   CONSTRAINT 'fk_ete_eta'
     FOREIGN KEY ('ete_eta_id' )
-    REFERENCES etapa ('eta_id' )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES etapa ('eta_id' ),
   CONSTRAINT 'fk_ete_exe'
     FOREIGN KEY ('ete_exe_id' )
-    REFERENCES exercicio ('exe_id' )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES exercicio ('exe_id' ));
 
 
 -- -----------------------------------------------------
 -- Table anaerobico
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS anaerobico (
-  'ana_id' INT NOT NULL ,
-  'ana_repeticoes' INT NOT NULL ,
+  'ana_id' INTEGER,
+  'ana_repeticoes' INTEGER NOT NULL ,
   'ana_peso' DECIMAL(5,2) NOT NULL ,
-  'ana_serie' INT NOT NULL ,
-  'ana_ete_id' INT NOT NULL ,
+  'ana_serie' INTEGER NOT NULL ,
+  'ana_ete_id' INTEGER NOT NULL ,
   PRIMARY KEY ('ana_id') ,
   CONSTRAINT 'fk_ana_ete'
     FOREIGN KEY ('ana_ete_id' )
-    REFERENCES etapa_exercicio ('ete_id' )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES etapa_exercicio ('ete_id' ));
 
 
 -- -----------------------------------------------------
 -- Table exercicio_realizado
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS exercicio_realizado (
-  'exr_id' INT NOT NULL ,
-  'exr_data_realizacao' INT NOT NULL ,
-  'exr_ete_id' INT NOT NULL ,
+  'exr_id' INTEGER,
+  'exr_data_realizacao' INTEGER NOT NULL ,
+  'exr_ete_id' INTEGER NOT NULL ,
   PRIMARY KEY ('exr_id') ,
   CONSTRAINT 'fk_exr_ete'
     FOREIGN KEY ('exr_ete_id' )
-    REFERENCES etapa_exercicio ('ete_id' )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES etapa_exercicio ('ete_id' ));
 
 
 -- -----------------------------------------------------
 -- Table exercicio_aerobico_realizado
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS exercicio_aerobico_realizado (
-  'ear_id' INT NOT NULL ,
-  'ear_duracao' INT NOT NULL ,
+  'ear_id' INTEGER,
+  'ear_duracao' INTEGER NOT NULL ,
   'ear_distancia' DECIMAL(5,2) NOT NULL ,
   'ear_velocidade' DECIMAL(5,2) NOT NULL ,
-  'ear_exr_id' INT NOT NULL ,
+  'ear_exr_id' INTEGER NOT NULL ,
   PRIMARY KEY ('ear_id') ,
   CONSTRAINT 'fk_Exercicio_Aerobico_Realizado_Exercicio_realizado1'
     FOREIGN KEY ('ear_exr_id' )
-    REFERENCES exercicio_realizado ('exr_id' )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    REFERENCES exercicio_realizado ('exr_id' ));
