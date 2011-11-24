8baac4b973f225af62979fa643e43a640828223d
ataide.azevedo@gmail.com <ataide.azevedo@gmail.com@740cbde1-dae2-8632-a52b-12e60ba9706f>
2011-11-24 14:22:33 +0000

    Barra de Progresso no Splash; Adicionado String Carregando...;

--- trunk/fitness-mobile/FitnessMobileAndroid/src/br/com/fitnessmobile/view/SplashScreen.java
+++ trunk/fitness-mobile/FitnessMobileAndroid/src/br/com/fitnessmobile/view/SplashScreen.java
@@ -1,26 +1,44 @@
 package br.com.fitnessmobile.view;
 
-import br.com.fitnessmobile.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.widget.ProgressBar;
+import br.com.fitnessmobile.R;
 
 public class SplashScreen extends Activity implements Runnable {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
-        
-        Handler h = new Handler();
-		h.postDelayed(this, 5000);//aqui é definido o delay do handler em milisegundos
-		
+	/** Called when the activity is first created. */
+
+	private ProgressBar progressbar;
+	private Thread t;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+
+		progressbar = (ProgressBar)findViewById(R.id.progressBar);
+		t = new Thread(SplashScreen.this);
+		t.start();
 	}
-	
-	public void run(){
+
+	private void chamaMain(){
 		startActivity(new Intent(this, FitnessMobileMain.class));
 		finish();
 	}
-    
+
+	public void run(){
+		int i = 1;
+		try {
+			while (i<=100){
+				Thread.sleep(50);
+				progressbar.setProgress(i++);
+				if(i >= 100){
+					chamaMain();
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 }
